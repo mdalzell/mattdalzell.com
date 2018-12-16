aec6ac5c5d5456f74e55600b46dc04fa1fb4f0ba
Matt Dalzell <matt.a.dalzell@gmail.com>
2018-12-16 15:16:56 -0600

    Added tags list to main blog page

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -2,21 +2,32 @@ import React from 'react'
 import { Link, graphql } from "gatsby";
 import Layout from '../components/layout'
 
-const BlogPage = ({ data }) => (
+const BlogPage = ({ 
+  data: {
+    allMarkdownRemark: { edges, group }
+  } 
+}) => (
   <Layout>
     <div>
         <h1>Blog</h1>
+        <h4>Recent Posts</h4>
         <ul className="no-list-style">
-          {data.allMarkdownRemark.edges.map(({ node }) => (
+          {edges.map(({ node }) => (
               <li key={node.id}>
                   <Link to={"/blog" + node.fields.slug}>{node.frontmatter.title + " - " + node.frontmatter.date}</Link>
               </li>
           ))}
         </ul>
+        <h4>Tags</h4>
+        <p>
+          {
+            group.map((tag) => (<a className="tag">{"#" + tag.fieldValue}</a>))
+          }
+        </p>
         <Link to="/">Return Home</Link>
     </div>
   </Layout>
-);
+)
 
 export const query = graphql`
   query IndexQuery {
@@ -29,10 +40,15 @@ export const query = graphql`
             slug
           }
           frontmatter {
+            tags
             title
             date(formatString: "DD MMMM, YYYY")
           }
         }
+      }
+      group(field: frontmatter___tags) {
+        fieldValue
+        totalCount
       }
     }
   }
