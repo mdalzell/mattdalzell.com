ac2101cbb094de88b3edf161e679180d03fea72a
mdalzell <matt.a.dalzell@gmail.com>
2019-02-23 18:56:11 -0600

    Updated header styling

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -1,28 +1,28 @@
-import React from "react";
-import { Link, graphql } from "gatsby";
+import React from 'react'
+import { Link, graphql } from 'gatsby'
 import Layout from '../components/layout'
 
 const BlogPost = ({ data }) => {
-    const post = data.markdownRemark;
-    return ( 
-      <Layout>
-        <div>
-            <h1>{post.frontmatter.title}</h1>
-            <h4>{post.frontmatter.date}</h4>
-            <div dangerouslySetInnerHTML={{ __html: post.html }} />
-            <p>
-              ~ MAD
-            </p>
-            <p>
-              {post.frontmatter.tags.map((tag) => (
-                <Link to={"/blog?tag=" + tag} className="tag">{"#" + tag}</Link>
-              ))}
-            </p>
-            <Link to="/blog">Return to Blog</Link>
-        </div>
-      </Layout>
-    );
-};
+  const post = data.markdownRemark
+  return (
+    <Layout>
+      <div>
+        <h1>{post.frontmatter.title}</h1>
+        <h3>{post.frontmatter.date}</h3>
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+        <p>~ MAD</p>
+        <p>
+          {post.frontmatter.tags.map(tag => (
+            <Link to={'/blog?tag=' + tag} className="tag">
+              {'#' + tag}
+            </Link>
+          ))}
+        </p>
+        <Link to="/blog">Return to Blog</Link>
+      </div>
+    </Layout>
+  )
+}
 
 export const query = graphql`
   query BlogPostQuery($slug: String!) {
@@ -35,6 +35,6 @@ export const query = graphql`
       }
     }
   }
-`;
+`
 
 export default BlogPost
