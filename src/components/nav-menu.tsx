86ca062f7c1a8dfe352fc865c8c14eb78b47586d
Matt Dalzell <contact@mattdalzell.com>
2020-08-25 21:55:13 -0500

    Add About page (#15)

--- src/components/nav-menu.tsx
+++ src/components/nav-menu.tsx
@@ -8,6 +8,9 @@ const NavMenu = () => {
         <Link to="/">Home</Link>
       </li>
       <li>
+        <Link to="/about">About</Link>
+      </li>
+      <li>
         <Link to="/blog">Blog</Link>
       </li>
       <li>
