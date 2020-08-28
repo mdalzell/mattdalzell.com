3ce0327318ade2bb13ba207f64bb00a4ee36a8d5
Matt Dalzell <contact@mattdalzell.com>
2020-08-28 18:07:28 -0500

    Markdown links open in new tab (#17)

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,5 +29,19 @@ module.exports = {
         path: `${__dirname}/src/content`,
       },
     },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: "gatsby-remark-external-links",
+            options: {
+              target: "_blank",
+              rel: "noopener noreferrer"
+            }
+          }
+        ]
+      }
+    }
   ],
 }
