72d4e63c0747842388201c708ef3ad2a4a1c38df
Matt Dalzell <contact@mattdalzell.com>
2026-04-11 13:50:57 -0500

    upgrade gatsby v5 (#53)
    * fix lockfile
    
    * upgrade to gatsby v5

--- __mocks__/gatsby.js
+++ __mocks__/gatsby.js
@@ -1,5 +1,5 @@
 const React = require('react')
-const gatsby = jest.requireActual('gatsby')
+const gatsby = jest.genMockFromModule('gatsby')
 
 module.exports = {
   ...gatsby,
--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     `gatsby-plugin-typescript`,
     `gatsby-transformer-remark`,
-    `gatsby-plugin-netlify-cms`,
+    `gatsby-plugin-decap-cms`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
